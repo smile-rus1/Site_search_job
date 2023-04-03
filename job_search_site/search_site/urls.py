e0be5c2540e4afcec7d8aa90fc3414dc7bc2cdf3
Gasak Ruslan <rusya.gasak@mail.ru>
2023-04-03 11:21:19 +0300

    созданы шаблоны: регистрации пользователей; войти пользователям

--- job_search_site/search_site/urls.py
+++ job_search_site/search_site/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 
-from .views import index
+from .views import index, user_login, logout_user, register
 
 
 urlpatterns = [
     path("", index, name='index'),
+    # user
+    path("user_login/", user_login, name="user_login"),
+    path("logout/", logout_user, name="logout_user"),
+    path("register/", register, name="register"),
+
 ]
--- job_search_site/search_site/views.py
+++ job_search_site/search_site/views.py
@@ -1,7 +1,39 @@
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .models import Applicant, Job, Company, Application
 
 
 def index(request):
     return render(request, "index.html")
 
+
+def user_login(request):
+    if request.user.is_authenticated:
+        return redirect("index")
+    else:
+        if request.method == "POST":
+            username = request.POST["username"]
+            password = request.POST["password"]
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                user1 = Applicant.objects.get(user=user)
+                if user1.type == "applicant":
+                    login(request, user)
+                    return redirect("user_homepage")
+            else:
+                thank = True
+                return render(request, "user_login.html", {"thank": thank})
+
+    return render(request, "user_login.html")
+
+
+def register(request):
+
+    return render(request, "register.html")
+
+def logout_user(request):
+    logout(request)
+    return redirect("index")
