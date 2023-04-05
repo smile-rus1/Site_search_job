b69222bd4e5ece578d0ccae83eccd68eb6240568
Gasak Ruslan <rusya.gasak@mail.ru>
2023-04-05 09:21:54 +0300

    добавлены urlы для компании

--- job_search_site/search_site/urls.py
+++ job_search_site/search_site/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from .views import index, user_login, logout_user, register, user_home_page
-
+from .views import index, user_login, logout_user, register, user_home_page, company_register, company_login, \
+    company_home_page
 
 urlpatterns = [
     path("", index, name='index'),
@@ -9,6 +9,10 @@ urlpatterns = [
     path("user_login/", user_login, name="user_login"),
     path("logout/", logout_user, name="logout_user"),
     path("register/", register, name="register"),
-    path("user_home_page/", user_home_page, name="user_home_page",)
+    path("user_home_page/", user_home_page, name="user_home_page"),
 
+    # company
+    path("company_login/", company_login, name="company_login"),
+    path("company_register/", company_register, name="company_register"),
+    path("company_home_page/", company_home_page, name="company_home_page"),
 ]
