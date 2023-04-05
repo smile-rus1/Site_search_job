ff7930fe240c6ee83e11401eb4ecbc880259a85b
Gasak Ruslan <rusya.gasak@mail.ru>
2023-04-05 12:18:08 +0300

    зарегистрировал модели в админке

--- job_search_site/search_site/admin.py
+++ job_search_site/search_site/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Applicant, Job, Company, Application
 
-# Register your models here.
+
+admin.site.register(Applicant)
+admin.site.register(Company)
+admin.site.register(Job)
+admin.site.register(Application)
