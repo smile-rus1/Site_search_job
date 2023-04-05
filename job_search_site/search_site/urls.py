38c690c93dee6a8278246a533b65c97e3a44602e
Gasak Ruslan <rusya.gasak@mail.ru>
2023-04-05 12:18:53 +0300

    добавлен маршрут к админке

--- job_search_site/search_site/urls.py
+++ job_search_site/search_site/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from .views import index, user_login, logout_user, register, user_home_page, company_register, company_login, \
-    company_home_page
+    company_home_page, admin_login, all_companies, delete_company
 
 urlpatterns = [
     path("", index, name='index'),
@@ -15,4 +15,9 @@ urlpatterns = [
     path("company_login/", company_login, name="company_login"),
     path("company_register/", company_register, name="company_register"),
     path("company_home_page/", company_home_page, name="company_home_page"),
+
+    # admin
+    path("admin_login/", admin_login, name="admin_register"),
+    path("all_companies/", all_companies, name="all_companies"),
+    path("delete_company/<int:myid>/", delete_company, name="delete_company"),
 ]
