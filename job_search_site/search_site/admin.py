ab6411d05f994f22883a1427db8863468a6975d0
Gasak Ruslan <rusya.gasak@mail.ru>
2023-04-11 22:19:54 +0300

    зарегестирована новая модель News

--- job_search_site/search_site/admin.py
+++ job_search_site/search_site/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Applicant, Job, Company, Application
+from .models import Applicant, Job, Company, Application, News
 
 
 admin.site.register(Applicant)
 admin.site.register(Company)
 admin.site.register(Job)
 admin.site.register(Application)
+admin.site.register(News)
