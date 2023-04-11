ee4362a8f3e2dadb9f2d3035b6b5983a5154e6c7
Gasak Ruslan <rusya.gasak@mail.ru>
2023-04-11 20:18:08 +0300

    добавлен маршрут в url

--- job_search_site/search_site/urls.py
+++ job_search_site/search_site/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import index, user_login, logout_user, register, user_home_page, company_register, company_login, \
     company_home_page, admin_login, all_companies, delete_company, change_status, accepted_company, rejected_company, \
     pending_company, all_applicant, delete_applicant, all_jobs, job_details, add_job, job_list, \
-    all_applicants_for_company, job_apply, edit_job
+    all_applicants_for_company, job_apply, edit_job, news_user, news_company
 
 urlpatterns = [
     path("", index, name='index'),
@@ -15,6 +15,7 @@ urlpatterns = [
     path("all_jobs/", all_jobs, name="all_jobs"),
     path("job_details/<int:job_id>", job_details, name="job_details"),
     path("job_apply/<int:myid>", job_apply, name="job_apply"),
+    path("news_user", news_user, name="news_user"),
 
     # company
     path("company_login/", company_login, name="company_login"),
@@ -24,6 +25,7 @@ urlpatterns = [
     path("edit_job/<int:id_job>", edit_job, name="edit_job"),
     path("job_list/", job_list, name="job_list"),
     path("all_applicants_for_company/", all_applicants_for_company, name="all_applicants_for_company"),
+    path("news_company", news_company, name="news_company"),
 
     # admin
     path("admin_login/", admin_login, name="admin_login"),
